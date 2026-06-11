d26e89f996e8de997623021c45dcad3c9ac4f4bd
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-06-11 14:42:01 +0530

    .

--- lecture_9/basic.js
+++ lecture_9/basic.js
@@ -1,1 +1,19 @@
-var
+const n = 5;
+
+let pattern = "";
+
+// Upper half
+for (let i = 1; i <= n; i++) {
+	const stars = "* ".repeat(i);
+	const spaces = "  ".repeat(2 * (n - i));
+	pattern += stars + spaces + stars + "\n";
+}
+
+// Lower half
+for (let i = n; i >= 1; i--) {
+	const stars = "* ".repeat(i);
+	const spaces = "  ".repeat(2 * (n - i));
+	pattern += stars + spaces + stars + "\n";
+}
+
+console.log(pattern);
