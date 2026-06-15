68751cd1fb2c6619bf9eb68f54d8a5811896de06
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-06-15 15:01:43 +0530

    .

--- lecture_11/my.js
+++ lecture_11/my.js
@@ -1,9 +1,13 @@
-
 // Array 
 let library = [];
 
 // add a book
-function addBook(book) {
+function addBook(bookName, rentPrice) {
+    let book = {
+        name: bookName,
+        rentPrice: rentPrice,
+        isRented: false
+    };
     library.push(book);
     console.log(library);
 }
@@ -15,34 +19,59 @@ function displayBooks() {
     } else {
         console.log("Books in Library:");
         for (let i = 0; i < library.length; i++) {
-            console.log((i + 1) + ". " + library[i]);
+            let status = library[i].isRented ? " (Rented)" : " (Available)";
+            console.log((i + 1) + ". " + library[i].name + " - Rent: ₹" + library[i].rentPrice + status);
         }
     }
 }
 
 // find()
 function searchBook(bookName) {
-    let found = library.find(b => b === bookName);
+    let found = library.find(b => b.name === bookName);
     if (found) {
-        console.log("Book found: " + found);
+        console.log("Book found: " + found.name);
     } else {
         console.log("Book not found.");
     }
 }
 
-function popbook(){
-    let removedElement = library.pop();
+// rent a book
+function rentBook(bookName, days) {
+    let book = library.find(b => b.name === bookName);
+    if (book) {
+        if (!book.isRented) {
+            book.isRented = true;
+            let totalRent = book.rentPrice * days;
+            console.log(`You rented "${book.name}" for ${days} days. Total rent: ₹${totalRent}`);
+        } else {
+            console.log(`Sorry, "${book.name}" is already rented.`);
+        }
+    } else {
+        console.log("Book not found.");
+    }
 }
 
-addBook("Book1");
-addBook("Book2");
-addBook("Book3");
+// remove last book
+function popbook(){
+    let removedElement = library.pop();
+    console.log("Removed book:", removedElement ? removedElement.name : "None");
+}
+
+// Example usage
+addBook("Book1", 10);
+addBook("Book2", 15);
+addBook("Book3", 20);
 displayBooks();
+
 searchBook("Book2");   
-addBook("Book4");
-addBook("Book5");
-addBook("Book6");  
+rentBook("Book2", 5);   
+displayBooks();
+
+addBook("Book4", 12);
+addBook("Book5", 18);
+addBook("Book6", 25);  
 searchBook("Book5");
 displayBooks();
+
 popbook();
 displayBooks();
