6308224933e48a42c3d8ac390aa48060648261d7
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-06-17 12:20:02 +0530

    js

---
+++ lecture_11/4.js
@@ -0,0 +1,11 @@
+const users = [
+    {name:"A", age:16},
+    {name:"B", age:20},
+    {name:"C", age:17},
+    {name:"D", age:22}
+]
+
+const remove  = users.filter(user => user.age < 18);
+console.log(remove);
+
+console.log(users);
