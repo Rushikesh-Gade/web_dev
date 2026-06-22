d80ead2eb67f2a4a6ccfa49cc7a1dfc5b035c04a
Rushikesh-Gade <rushikeshgade093@gmail.com>
2026-06-22 14:33:51 +0530

    ,

---
+++ java/task.js
@@ -0,0 +1,26 @@
+function startDownload(callback) {
+    console.log("Download Started...");
+
+    setTimeout(() => {
+        callback();
+    }, 3000);
+}
+
+function downloading(callback) {
+    console.log("Downloading...");
+
+    setTimeout(() => {
+        callback();
+    }, 2000);
+}
+
+function downloadCompleted() {
+    console.log("Download Completed!");
+}
+
+
+startDownload(() => {
+    downloading(() => {
+        downloadCompleted();
+    });
+});
